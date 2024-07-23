eb2b6e4c34049a74cbafa752f63ec7dac68e7ba5
Lint Action <lint-action@samuelmeuli.com>
2024-07-23 11:57:16 +0000

    Fix code style issues with ESLint

--- index.js
+++ index.js
@@ -11,9 +11,8 @@
       year: 'numeric',
       weekday: 'numeric',
     }
-    
 
-    const a=0;
+    const a = 0
     const now = new Date()
     const formattedDate = now.toLocaleDateString('en-US', options)
     return `Hello, ${name}! Today is ${formattedDate}.`
